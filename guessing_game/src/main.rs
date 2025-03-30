0db731f7e9bc291ad39d21359323e5f22c8322f1
Dan Johansson <danjohansson98@gmail.com>
2025-03-30 11:19:28 +0200

    updated readme with progress on ch03-05.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -27,8 +27,6 @@ fn main() {
 
         println!("You guessed: {guess}");
 
-        // --snip--
-
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
